fd73f761de4669c0d06f7f60ad74b09db82f2f1e
Mads Nielsen <post@madsnielsen.dk>
2018-08-26 19:01:53 +0200

    init

---
+++ TemplateWorkbench/JsonConverters.cs
@@ -0,0 +1,72 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateWorkbench
+{
+    class JsonConverterDictionary : CustomCreationConverter<IDictionary<string, object>>
+    {
+        public override IDictionary<string, object> Create(Type objectType)
+        {
+            return new Dictionary<string, object>();
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            // in addition to handling IDictionary<string, object>
+            // we want to handle the deserialization of dict value
+            // which is of type object
+            return objectType == typeof(object) || base.CanConvert(objectType);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.StartObject
+                || reader.TokenType == JsonToken.Null)
+                return base.ReadJson(reader, objectType, existingValue, serializer);
+
+            // deserialize arrays as ICollection<object>
+            if (reader.TokenType == JsonToken.StartArray)
+                return serializer.Deserialize<ICollection<object>>(reader);
+
+            // if the next token is not an object
+            // then fall back on standard deserializer (strings, numbers etc.)
+            return serializer.Deserialize(reader);
+        }
+    }
+
+    class JsonConverterCollection : CustomCreationConverter<ICollection<object>>
+    {
+        public override ICollection<object> Create(Type objectType)
+        {
+            return new List<object>();
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            // in addition to handling ICollection<object>
+            // we want to handle the deserialization of dict value
+            // which is of type object
+            return objectType == typeof(object) || base.CanConvert(objectType);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.StartArray
+                || reader.TokenType == JsonToken.Null)
+                return base.ReadJson(reader, objectType, existingValue, serializer);
+
+            // deserialize objects as IDictionary<string,object>
+            if (reader.TokenType == JsonToken.StartObject)
+                return serializer.Deserialize<IDictionary<string,object>>(reader);
+
+            // if the next token is not an object
+            // then fall back on standard deserializer (strings, numbers etc.)
+            return serializer.Deserialize(reader);
+        }
+    }
+}
---
+++ TemplateWorkbench/MainWindow.xaml.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Antlr4.StringTemplate;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Dynamic;
+
+namespace TemplateWorkbench
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private string render = "";
+        public string Render {
+            get { return render; }
+        }
+
+        private DateTime? renderTime;
+        public string RenderTime
+        {
+            get { return renderTime?.ToLongTimeString() ?? "N/A"; }
+        }
+
+        public ObservableCollection<string> Templates { get; set; } = new ObservableCollection<string>();
+
+        private string templateStatus;
+        public string TemplateStatus
+        {
+            get { return templateStatus; }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.R && (
+                e.KeyboardDevice.IsKeyDown(Key.LeftCtrl) || e.KeyboardDevice.IsKeyDown(Key.RightCtrl)))
+            {
+                // CTRL+R
+                PerformRender();
+            }
+            else if (e.Key == Key.F && (
+                e.KeyboardDevice.IsKeyDown(Key.LeftCtrl) || e.KeyboardDevice.IsKeyDown(Key.RightCtrl)))
+            {
+                // CTRL+F
+                FormatJson();
+            }
+        }
+
+        private void FormatJson()
+        {
+            try
+            {
+                var prettified = JsonConvert.SerializeObject(JsonConvert.DeserializeObject(txtDataModel.Text), Formatting.Indented);
+                txtDataModel.Text = prettified;
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void PerformRender()
+        {
+            try
+            {
+                // Load template
+                TemplateGroupString stg = new TemplateGroupString(txtTemplate.Text);
+                var templateNames = stg.GetTemplateNames();
+                templateNames.ToList().ForEach(t => {
+                    if (!Templates.Contains(t)) Templates.Add(t);
+                });
+                Templates.ToList().ForEach(t =>
+                {
+                    if (!templateNames.Contains(t)) Templates.Remove(t);
+                });
+                var templateInst = stg.GetInstanceOf(lbTemplates.SelectedValue as string);
+
+                // Load data model
+                JObject json = JObject.Parse(txtDataModel.Text);
+                var jsonDict = JsonConvert.DeserializeObject<IDictionary<string, object>>(txtDataModel.Text, new JsonConverter[] { new JsonConverterDictionary(), new JsonConverterCollection() });
+                foreach (JProperty child in json.Children())
+                {
+                    object childValue;
+                    jsonDict.TryGetValue(child.Name, out childValue);
+                    templateInst.Add(child.Name, childValue);
+                }
+
+                // Render
+                render = templateInst.Render();
+
+                templateStatus = "OK";
+            }
+            catch (Exception e)
+            {
+                templateStatus = e.Message;
+            }
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("TemplateStatus"));
+
+            //render = "Rendering " + PadWithDots(txtTemplate.Text, 40) + " with data " + PadWithDots(txtDataModel.Text, 40);
+            renderTime = DateTime.Now;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Render"));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("RenderTime"));
+        }
+
+        private string PadWithDots(string text, int length)
+        {
+            if (text.Length > length)
+                return text.Substring(0, length - 5) + "[...]";
+            else
+                return text;
+        }
+
+        private void lbTemplates_TargetUpdated(object sender, DataTransferEventArgs e)
+        {
+            ComboBox cmb = (sender as ComboBox);
+            (cmb.ItemsSource as INotifyCollectionChanged).CollectionChanged += (
+                (_,__) =>
+                    {
+                        if ((sender as ComboBox).SelectedIndex == -1 && (sender as ComboBox).Items.Count > 0)
+                            (sender as ComboBox).SelectedIndex = 0;
+                    }
+            );
+        }
+
+        private void wnd_Closing(object sender, CancelEventArgs e)
+        {
+            e.Cancel = (MessageBox.Show("Are you sure you want to close?", "Close", MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.No) == MessageBoxResult.No);
+        }
+    }
+}
