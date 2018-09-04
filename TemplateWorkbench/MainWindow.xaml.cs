d5c1eac76626f4131a38c3f6504813d60d0b4ea2
Mads Nielsen <post@madsnielsen.dk>
2018-09-04 18:08:52 +0200

    M-V-VM serialization indication of deleted templates tests

--- TemplateWorkbench/MainWindow.xaml.cs
+++ TemplateWorkbench/MainWindow.xaml.cs
@@ -13,124 +13,28 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Antlr4.StringTemplate;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
-using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
-using System.Dynamic;
+using System.Globalization;
 
 namespace TemplateWorkbench
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window, INotifyPropertyChanged
+    public partial class MainWindow : Window
     {
-        private string render = "";
-        public string Render {
-            get { return render; }
-        }
-
-        private DateTime? renderTime;
-        public string RenderTime
-        {
-            get { return renderTime.HasValue ? renderTime?.ToLongTimeString() + " (" + TimeFormatter.TimeAgo(renderTime.Value) + ")" : "N/A"; }
-        }
-
-        public ObservableCollection<string> Templates { get; set; } = new ObservableCollection<string>();
-
-        private string templateStatus;
-        public string TemplateStatus
-        {
-            get { return templateStatus; }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        private System.Timers.Timer timer = new System.Timers.Timer();
+        private static readonly string fileDialogFilter = $"Project files (*.{Model.defaultProjectExtension})|*.{Model.defaultProjectExtension}|All files (*.*)|*.*";
 
         public MainWindow()
         {
             InitializeComponent();
-
-            timer.Elapsed += (object sender, System.Timers.ElapsedEventArgs e) => { PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("RenderTime")); };
-            timer.Interval = 10000;
-            timer.Enabled = true;
-        }
-
-        private void FormatJson()
-        {
-            try
-            {
-                var prettified = JsonConvert.SerializeObject(JsonConvert.DeserializeObject(txtDataModel.Text), Formatting.Indented);
-                txtDataModel.Text = prettified;
-            }
-            catch (Exception)
-            {
-            }
-        }
-
-        private void PerformRender()
-        {
-            try
-            {
-                // Load template
-                TemplateGroupString stg = new TemplateGroupString(txtTemplate.Text);
-                var templateNames = stg.GetTemplateNames();
-                templateNames.ToList().ForEach(t => {
-                    if (!Templates.Contains(t)) Templates.Add(t);
-                });
-                Templates.ToList().ForEach(t =>
-                {
-                    if (!templateNames.Contains(t)) Templates.Remove(t);
-                });
-                var templateInst = stg.GetInstanceOf(lbTemplates.SelectedValue as string);
-
-                // Load data model
-                if (templateInst.GetAttributes() != null)
-                {
-                    var jsonDict = JsonConvert.DeserializeObject<IDictionary<string, object>>(txtDataModel.Text, new JsonConverter[] { new JsonConverterDictionary(), new JsonConverterCollection() });
-                    foreach (var elem in jsonDict)
-                    {
-                        if (templateInst.GetAttributes().ContainsKey(elem.Key)) {
-                            templateInst.Add(elem.Key, elem.Value);
-                        }
-                    }
-                }
-
-                // Render
-                render = templateInst.Render();
-
-                templateStatus = "OK";
-                renderTime = DateTime.Now;
-                timer.Stop(); timer.Start();
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Render"));
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("RenderTime"));
-            }
-            catch (Exception e)
-            {
-                templateStatus = e.Message;
-            }
-
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("TemplateStatus"));
         }
 
         private void CopyToClipBoard(string text)
         {
             Clipboard.SetText(text);
-        }
-
-        private void lbTemplates_TargetUpdated(object sender, DataTransferEventArgs e)
-        {
-            ListBox cmb = (sender as ListBox);
-            (cmb.ItemsSource as INotifyCollectionChanged).CollectionChanged += (
-                (_,__) =>
-                    {
-                        if ((sender as ListBox).SelectedIndex == -1 && (sender as ListBox).Items.Count > 0)
-                            (sender as ListBox).SelectedIndex = 0;
-                    }
-            );
         }
 
         private void wnd_Closing(object sender, CancelEventArgs e)
@@ -142,7 +46,8 @@ namespace TemplateWorkbench
 
         private void CommandRefresh_Executed(object sender, ExecutedRoutedEventArgs e)
         {
-            PerformRender();
+            viewModel.CompileTemplates();
+            viewModel.PerformRender();
         }
 
         private void CommandExit_Executed(object sender, ExecutedRoutedEventArgs e)
@@ -152,17 +57,51 @@ namespace TemplateWorkbench
 
         private void CommandFormatData_Executed(object sender, ExecutedRoutedEventArgs e)
         {
-            FormatJson();
+            viewModel.FormatJson();
         }
 
         private void CommandCopyToClipBoard_Executed(object sender, ExecutedRoutedEventArgs e)
         {
-            CopyToClipBoard(render);
+            CopyToClipBoard(viewModel.Render);
+        }
+
+        private void CommandSaveAs_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            var dialog = new Microsoft.Win32.SaveFileDialog() { Filter = fileDialogFilter };
+            if (dialog.ShowDialog() ?? false)
+            {
+                viewModel.SaveAs(dialog.FileName);
+            }
+        }
+
+        private void CommandSave_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (!viewModel.Save())
+                Commands.SaveAs.Execute(null, this);
+        }
+
+        private void CommandOpen_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            var dialog = new Microsoft.Win32.OpenFileDialog() { Filter = fileDialogFilter, CheckFileExists = true, Multiselect = false };
+            if (dialog.ShowDialog() ?? false)
+            {
+                viewModel.Open(dialog.FileName);
+            }
         }
     }
 
     public static class Commands
     {
+        public static readonly RoutedUICommand Open = new RoutedUICommand(
+            "_Open",
+            "Open",
+            typeof(Commands),
+            new InputGestureCollection()
+            {
+                new KeyGesture(Key.O, ModifierKeys.Control)
+            }
+        );
+
         public static readonly RoutedUICommand Refresh = new RoutedUICommand(
             "_Refresh",
             "Refresh",
@@ -170,6 +109,22 @@ namespace TemplateWorkbench
             new InputGestureCollection() {
                 new KeyGesture(Key.R, ModifierKeys.Control)
             }
+        );
+
+        public static readonly RoutedUICommand Save = new RoutedUICommand(
+            "_Save",
+            "Save",
+            typeof(Commands),
+            new InputGestureCollection()
+            {
+                new KeyGesture(Key.S, ModifierKeys.Control)
+            }
+        );
+
+        public static readonly RoutedUICommand SaveAs = new RoutedUICommand(
+            "Save _as",
+            "Save as",
+            typeof(Commands)
         );
 
         public static readonly RoutedUICommand Exit = new RoutedUICommand(
---
+++ TemplateWorkbench/ViewModel.cs
@@ -0,0 +1,177 @@
+﻿using Antlr4.StringTemplate;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateWorkbench
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string DataModel { get; set; }
+        public string LastRender { get; private set; }
+        private Model model;
+        public string Render { get; private set; }
+        public DateTime RenderTime { get; private set; }
+        public string StatusDataModel { get; private set; }
+        public string StatusTemplate { get; private set; }
+        protected TemplateGroup stg;
+        public string Template { get; set; }
+        public ObservableCollection<TemplateListItem> Templates { get; set; } = new ObservableCollection<TemplateListItem>();
+        public TemplateListItem TemplateSelected { get; set; }
+        private System.Timers.Timer timer = new System.Timers.Timer();
+        private readonly int timerDefaultInterval = 1000;
+
+        public ViewModel()
+        {
+            timer.Elapsed += (_, __) => {
+                if (RenderTime != null)
+                {
+                    RefreshLastRender();
+                    timer.Interval = Math.Max(DateTime.Now.Subtract(RenderTime).Milliseconds / 60, timerDefaultInterval);
+                }
+                else
+                {
+                    LastRender = "N/A";
+                }
+            };
+        }
+
+        public void CompileTemplates()
+        {
+            HashSet<string> templateNames = new HashSet<string>();
+            try
+            {
+                stg = new TemplateGroupString(Template);
+                templateNames = stg.GetTemplateNames();
+                StatusTemplate = "Template(s) compiled";
+            }
+            catch (Exception e)
+            {
+                StatusTemplate = $"Error compiling template ({e.Message})";
+            }
+
+            // update templates list
+            foreach (var t in templateNames)
+            {
+                if (Templates.Count(tp => tp.Name == t) == 0) Templates.Add(new TemplateListItem() { Name = t });
+            }
+            foreach (var tp in Templates)
+            {
+                if (tp.Deleted) Templates.Remove(tp);
+                if (!templateNames.Contains(tp.Name)) tp.Deleted = true;
+            }
+
+            // if no or invalid selection, select the first valid template if it exists
+            if (TemplateSelected == null || TemplateSelected.Deleted)
+            {
+                TemplateSelected = Templates.FirstOrDefault(t => !t.Deleted);
+            }
+        }
+
+        public void FormatJson()
+        {
+            try
+            {
+                var prettified = JsonConvert.SerializeObject(JsonConvert.DeserializeObject(DataModel), Formatting.Indented);
+                DataModel = prettified;
+            }
+            catch (Exception e)
+            {
+                StatusDataModel = $"JSON format failed ({e.Message})";
+            }
+        }
+
+        public void Open(string path)
+        {
+            model = Model.LoadProject(path);
+            DataModel = model.DataModel;
+            Template = model.TemplateCode;
+            CompileTemplates();
+        }
+
+        public void PerformRender()
+        {
+            if (TemplateSelected != null)
+            {
+                try
+                {
+                    // get template instance
+                    var templateInst = stg.GetInstanceOf(TemplateSelected.Name);
+
+                    // Load data model
+                    if (templateInst.GetAttributes() != null)
+                    {
+                        var jsonDict = JsonConvert.DeserializeObject<IDictionary<string, object>>(DataModel, new JsonConverter[] { new JsonConverterDictionary(), new JsonConverterCollection() });
+                        foreach (var elem in jsonDict)
+                        {
+                            if (templateInst.GetAttributes().ContainsKey(elem.Key))
+                            {
+                                templateInst.Add(elem.Key, elem.Value);
+                            }
+                        }
+                    }
+                    StatusDataModel = "OK";
+
+                    // Render
+                    Render = templateInst.Render();
+
+                    StatusTemplate = "OK";
+                    RenderTime = DateTime.Now;
+                    RefreshLastRender();
+                    timer.Interval = timerDefaultInterval;
+                    timer.Stop(); timer.Start();
+                }
+                catch (JsonSerializationException e)
+                {
+                    StatusDataModel = $"Error reading data model ({e.Message})";
+                }
+                catch (Exception e)
+                {
+                    StatusTemplate = e.Message;
+                }
+            }
+            else
+            {
+                StatusTemplate = "No template selected";
+            }
+        }
+
+        public void RefreshLastRender()
+        {
+            LastRender = RenderTime.ToLongTimeString() + " (" + TimeFormatter.TimeAgo(RenderTime) + ")";
+        }
+
+        public bool Save()
+        {
+            if (model != null)
+            {
+                model.DataModel = DataModel;
+                model.TemplateCode = Template;
+                model.SaveProject();
+                return true;
+            }
+            return false;
+        }
+
+        public void SaveAs(string path)
+        {
+            model = new Model() { DataModel = DataModel, TemplateCode = Template, projectPath = path };
+            model.SaveProject();
+        }
+    }
+
+    class TemplateListItem : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Name { get; set; }
+        public bool Deleted { get; set; } = false;
+    }
+}
